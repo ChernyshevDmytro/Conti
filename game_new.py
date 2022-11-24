58c8d32ba574da4f90884439c8e06b9168e2b39b
Shakal1985 <shakal1985@i.ua>
2022-11-24 21:35:50 +0200

    second

--- game_new.py
+++ game_new.py
@@ -2,9 +2,10 @@ import random
 from datetime import datetime
 import json
 import os
+
 # e - exit; r - rock, s - scissors; p - paper
 user_command = ["r", "s", "p", "e"]
-win_params = {0: " draw ", 2: "Computer win", -2: "You win", -1:"", 1:""}
+win_params = {0: " draw ", 2: "Computer win", -2: "You win", -1: "", 1: ""}
 """
 rock: 
 defense_from_rock = 0
@@ -29,14 +30,17 @@ game_save_name = "game_paper_scissors_rock.json"
 if os.path.getsize(game_save_name) > 0:
     with open(game_save_name, "r") as file:
         scores = json.load(file)
-        comp_win=0
-        you_win=0
+        comp_win = 0
+        you_win = 0
+
         for k, v in scores.items():
-          if v ==  'You win':
-             you_win+=1
-          elif v ==  'Computer win':
-             comp_win+=1             
+            if v == "You win":
+
+                you_win += 1
+            elif v == "Computer win":
+                comp_win += 1
         print(f"Computer wins {comp_win} times\nYou win {you_win} times")
+
 
 class rock_scissors_paper:
     def __init__(self, rsp):
@@ -44,24 +48,25 @@ class rock_scissors_paper:
 
     def __add__(self, other):
         message = ""
-        for i in range(len(self.coeff)):                
-            message+= win_params[self.coeff[i]-other.coeff[i]]
+
+        for i in range(len(self.coeff)):
+            message += win_params[self.coeff[i] - other.coeff[i]]
+
         scores[str(datetime.now())] = message
-        return (message)    
+        return message
 
 
 while True:
     user_input_command = input("Please, choose r, s, p or e for exit :")
     if user_input_command == "e":
-        with open(game_save_name, 'w') as f:
+        with open(game_save_name, "w") as f:
             json.dump(scores, f)
 
         break
     else:
         comp_choise = random.choice(user_command[:3])
         comp_choise_ins = rock_scissors_paper(comp_choise)
-        my_choice = rock_scissors_paper(user_input_command)        
+        
+        my_choice = rock_scissors_paper(user_input_command)
         print(f"Computer choose {comp_choise}")
         print(my_choice + comp_choise_ins)
-         
-
